4bdd91603c62d3ce0d3834935ac91d17a0b47028
darshan <darshan.pawar@druva.com>
2023-08-02 09:44:29 +0100

    Remove unnecessary console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript file is loaded!')
+// console.log('Client side javascript file is loaded!')
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
